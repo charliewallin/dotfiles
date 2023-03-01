a3bd8ec90c1bb004630d184a44471129fd254e0d
Charlie Wallin <charliekwallin@gmail.com>
2023-03-01 13:30:52 -0500

    Finished colors again. Everyting works. Playing it safe and merging to main before creating the fuzzy-find branch.

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -29,17 +29,17 @@ local use = require('packer').use
 use('wbthomason/packer.nvim')
 
 -- One Dark theme.
--- use({
---   'jessarcher/onedark.nvim',
---   config = function()
---     vim.cmd('colorscheme onedark')
+use({
+  'jessarcher/onedark.nvim',
+  config = function()
+    vim.cmd('colorscheme onedark')
 
---     vim.api.nvim_set_hl(0, 'FloatBorder', {
---       fg = vim.api.nvim_get_hl_by_name('NormalFloat', true).background,
---       bg = vim.api.nvim_get_hl_by_name('NormalFloat', true).background,
---     })
---   end,
--- })
+    vim.api.nvim_set_hl(0, 'FloatBorder', {
+      fg = vim.api.nvim_get_hl_by_name('NormalFloat', true).background,
+      bg = vim.api.nvim_get_hl_by_name('NormalFloat', true).background,
+    })
+  end,
+})
 
 -- Commenting support
 -- gcc to turn on and off comments
